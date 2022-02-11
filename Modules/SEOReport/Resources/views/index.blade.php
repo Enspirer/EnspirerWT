ce668e2742ecabd1cfbe125050e7309d3ec05d4c
sanjayaharshana <sanjayaharasana@gmail.com>
2022-02-11 11:24:13 +0530

    Create Core Modules

---
+++ Modules/SEOReport/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('seoreport::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('seoreport.name') !!}
+    </p>
+@endsection
