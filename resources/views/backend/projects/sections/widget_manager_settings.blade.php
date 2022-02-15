6665299f120d72c9298dced1d88e7d4bb389b292
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-02-15 12:04:18 +0530

    widget manager settings

--- resources/views/backend/projects/sections/widget_manager_settings.blade.php
+++ resources/views/backend/projects/sections/widget_manager_settings.blade.php
@@ -337,14 +337,214 @@
                                 <input class="form-check-input" type="checkbox" name="date_time" id="sunday" value="sunday">
                                 <label class="form-check-label" for="sunday">Sunday</label>
                             </div>
+                        
+
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group mt-1">
+                                        <label>Start Time <span class="text-danger">*</span></label>
+                                        <input type="date" id="start_time" class="form-control" name="start_time" required>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group mt-1">
+                                        <label>End Time <span class="text-danger">*</span></label>
+                                        <input type="date" id="end_time" class="form-control" name="end_time" required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group mt-1 mb-3">
+                                <label>TimeZone <span class="text-danger">*</span></label>
+
+                                <select class="form-control custom-select" name="timezone" required>
+                                    <option value="Etc/GMT+12">(GMT-12:00) International Date Line West</option>
+                                    <option value="Pacific/Midway">(GMT-11:00) Midway Island, Samoa</option>
+                                    <option value="Pacific/Honolulu">(GMT-10:00) Hawaii</option>
+                                    <option value="US/Alaska">(GMT-09:00) Alaska</option>
+                                    <option value="America/Los_Angeles">(GMT-08:00) Pacific Time (US & Canada)</option>
+                                    <option value="America/Tijuana">(GMT-08:00) Tijuana, Baja California</option>
+                                    <option value="US/Arizona">(GMT-07:00) Arizona</option>
+                                    <option value="America/Chihuahua">(GMT-07:00) Chihuahua, La Paz, Mazatlan</option>
+                                    <option value="US/Mountain">(GMT-07:00) Mountain Time (US & Canada)</option>
+                                    <option value="America/Managua">(GMT-06:00) Central America</option>
+                                    <option value="US/Central">(GMT-06:00) Central Time (US & Canada)</option>
+                                    <option value="America/Mexico_City">(GMT-06:00) Guadalajara, Mexico City, Monterrey</option>
+                                    <option value="Canada/Saskatchewan">(GMT-06:00) Saskatchewan</option>
+                                    <option value="America/Bogota">(GMT-05:00) Bogota, Lima, Quito, Rio Branco</option>
+                                    <option value="US/Eastern">(GMT-05:00) Eastern Time (US & Canada)</option>
+                                    <option value="US/East-Indiana">(GMT-05:00) Indiana (East)</option>
+                                    <option value="Canada/Atlantic">(GMT-04:00) Atlantic Time (Canada)</option>
+                                    <option value="America/Caracas">(GMT-04:00) Caracas, La Paz</option>
+                                    <option value="America/Manaus">(GMT-04:00) Manaus</option>
+                                    <option value="America/Santiago">(GMT-04:00) Santiago</option>
+                                    <option value="Canada/Newfoundland">(GMT-03:30) Newfoundland</option>
+                                    <option value="America/Sao_Paulo">(GMT-03:00) Brasilia</option>
+                                    <option value="America/Argentina/Buenos_Aires">(GMT-03:00) Buenos Aires, Georgetown</option>
+                                    <option value="America/Godthab">(GMT-03:00) Greenland</option>
+                                    <option value="America/Montevideo">(GMT-03:00) Montevideo</option>
+                                    <option value="America/Noronha">(GMT-02:00) Mid-Atlantic</option>
+                                    <option value="Atlantic/Cape_Verde">(GMT-01:00) Cape Verde Is.</option>
+                                    <option value="Atlantic/Azores">(GMT-01:00) Azores</option>
+                                    <option value="Africa/Casablanca">(GMT+00:00) Casablanca, Monrovia, Reykjavik</option>
+                                    <option value="Etc/Greenwich">(GMT+00:00) Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London</option>
+                                    <option value="Europe/Amsterdam">(GMT+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna</option>
+                                    <option value="Europe/Belgrade">(GMT+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague</option>
+                                    <option value="Europe/Brussels">(GMT+01:00) Brussels, Copenhagen, Madrid, Paris</option>
+                                    <option value="Europe/Sarajevo">(GMT+01:00) Sarajevo, Skopje, Warsaw, Zagreb</option>
+                                    <option value="Africa/Lagos">(GMT+01:00) West Central Africa</option>
+                                    <option value="Asia/Amman">(GMT+02:00) Amman</option>
+                                    <option value="Europe/Athens">(GMT+02:00) Athens, Bucharest, Istanbul</option>
+                                    <option value="Asia/Beirut">(GMT+02:00) Beirut</option>
+                                    <option value="Africa/Cairo">(GMT+02:00) Cairo</option>
+                                    <option value="Africa/Harare">(GMT+02:00) Harare, Pretoria</option>
+                                    <option value="Europe/Helsinki">(GMT+02:00) Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius</option>
+                                    <option value="Asia/Jerusalem">(GMT+02:00) Jerusalem</option>
+                                    <option value="Europe/Minsk">(GMT+02:00) Minsk</option>
+                                    <option value="Africa/Windhoek">(GMT+02:00) Windhoek</option>
+                                    <option value="Asia/Kuwait">(GMT+03:00) Kuwait, Riyadh, Baghdad</option>
+                                    <option value="Europe/Moscow">(GMT+03:00) Moscow, St. Petersburg, Volgograd</option>
+                                    <option value="Africa/Nairobi">(GMT+03:00) Nairobi</option>
+                                    <option value="Asia/Tbilisi">(GMT+03:00) Tbilisi</option>
+                                    <option value="Asia/Tehran">(GMT+03:30) Tehran</option>
+                                    <option value="Asia/Muscat">(GMT+04:00) Abu Dhabi, Muscat</option>
+                                    <option value="Asia/Baku">(GMT+04:00) Baku</option>
+                                    <option value="Asia/Yerevan">(GMT+04:00) Yerevan</option>
+                                    <option value="Asia/Kabul">(GMT+04:30) Kabul</option>
+                                    <option value="Asia/Yekaterinburg">(GMT+05:00) Yekaterinburg</option>
+                                    <option value="Asia/Karachi">(GMT+05:00) Islamabad, Karachi, Tashkent</option>
+                                    <option value="Asia/Calcutta">(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi</option>
+                                    <option value="Asia/Calcutta">(GMT+05:30) Sri Jayawardenapura</option>
+                                    <option value="Asia/Katmandu">(GMT+05:45) Kathmandu</option>
+                                    <option value="Asia/Almaty">(GMT+06:00) Almaty, Novosibirsk</option>
+                                    <option value="Asia/Dhaka">(GMT+06:00) Astana, Dhaka</option>
+                                    <option value="Asia/Rangoon">(GMT+06:30) Yangon (Rangoon)</option>
+                                    <option value="Asia/Bangkok">(GMT+07:00) Bangkok, Hanoi, Jakarta</option>
+                                    <option value="Asia/Krasnoyarsk">(GMT+07:00) Krasnoyarsk</option>
+                                    <option value="Asia/Hong_Kong">(GMT+08:00) Beijing, Chongqing, Hong Kong, Urumqi</option>
+                                    <option value="Asia/Kuala_Lumpur">(GMT+08:00) Kuala Lumpur, Singapore</option>
+                                    <option value="Asia/Irkutsk">(GMT+08:00) Irkutsk, Ulaan Bataar</option>
+                                    <option value="Australia/Perth">(GMT+08:00) Perth</option>
+                                    <option value="Asia/Taipei">(GMT+08:00) Taipei</option>
+                                    <option value="Asia/Tokyo">(GMT+09:00) Osaka, Sapporo, Tokyo</option>
+                                    <option value="Asia/Seoul">(GMT+09:00) Seoul</option>
+                                    <option value="Asia/Yakutsk">(GMT+09:00) Yakutsk</option>
+                                    <option value="Australia/Adelaide">(GMT+09:30) Adelaide</option>
+                                    <option value="Australia/Darwin">(GMT+09:30) Darwin</option>
+                                    <option value="Australia/Brisbane">(GMT+10:00) Brisbane</option>
+                                    <option value="Australia/Canberra">(GMT+10:00) Canberra, Melbourne, Sydney</option>
+                                    <option value="Australia/Hobart">(GMT+10:00) Hobart</option>
+                                    <option value="Pacific/Guam">(GMT+10:00) Guam, Port Moresby</option>
+                                    <option value="Asia/Vladivostok">(GMT+10:00) Vladivostok</option>
+                                    <option value="Asia/Magadan">(GMT+11:00) Magadan, Solomon Is., New Caledonia</option>
+                                    <option value="Pacific/Auckland">(GMT+12:00) Auckland, Wellington</option>
+                                    <option value="Pacific/Fiji">(GMT+12:00) Fiji, Kamchatka, Marshall Is.</option>
+                                    <option value="Pacific/Tongatapu">(GMT+13:00) Nuku'alofa</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="card px-3 custom-shadow border-0" style="-webkit-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); -moz-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); box-shadow: inset 0px 0.5px 14px -8px rgba(0,0,0,0.75);">
+                            <h6 class="card-header p-0 mt-0">Open Chat Triggers</h6>
+                            <div class="form-group mt-3">
+                                <label>Time on Page <span class="text-danger">*</span></label>
+                                <input type="text" id="time_on_page" class="form-control" name="time_on_page" required>
+                            </div>  
+                            
+                            <div class="form-group mt-1">
+                                <label>Time on Site <span class="text-danger">*</span></label>
+                                <input type="text" id="time_on_site" class="form-control" name="time_on_site" required>
+                            </div> 
+
+                            <div class="form-group mt-1">
+                                <label>Scroll Position <span class="text-danger">*</span></label>
+                                <input type="text" id="scroll_position" class="form-control" name="scroll_position" required>
+                            </div> 
+
+                            <div class="custom-control custom-switch mb-1">
+                                <input type="checkbox" class="custom-control-input" name="exit_internet" id="exit_internet">
+                                <label class="custom-control-label" for="exit_internet">Exit Internet</label>
+                            </div>
+
+                            <label class="mt-3">Notification <span class="text-danger">*</span></label>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" name="notification" id="bubble_notification_bage" value="bubble_notification_bage">
+                                <label class="form-check-label" for="bubble_notification_bage">Bubble Notification Badge</label>
+                            </div>
+                            <div class="form-check form-check-inline mb-3">
+                                <input class="form-check-input" type="checkbox" name="notification" id="show_notification_in_tab_tile" value="show_notification_in_tab_tile">
+                                <label class="form-check-label" for="show_notification_in_tab_tile">Show Notification in Tab Title</label>
+                            </div>
+
                         </div>
 
                     </div>
+                    
+
+                    <div class="tab-pane fade" id="appearance" role="tabpanel" aria-labelledby="appearance-tab">
+                        <div class="card px-3 custom-shadow border-0" style="-webkit-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); -moz-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); box-shadow: inset 0px 0.5px 14px -8px rgba(0,0,0,0.75);">
+                            <h6 class="card-header p-0 mt-0">Colors</h6>
+                            <div class="form-group mt-3">
+                                <label>Bubble background Color <span class="text-danger">*</span></label>
+                                <input type="color" id="bubble_background_color" class="form-control" name="bubble_background_color" required>
+                            </div> 
+                            <div class="form-group mt-1">
+                                <label>Bubble Icon Color <span class="text-danger">*</span></label>
+                                <input type="color" id="bubble_icon_color" class="form-control" name="bubble_icon_color" required>
+                            </div> 
+                            <div class="form-group mt-1">
+                                <label>Button Color <span class="text-danger">*</span></label>
+                                <input type="color" id="button_color" class="form-control" name="button_color" required>
+                            </div> 
+                            <div class="form-group mt-1">
+                                <label>Header Background Color <span class="text-danger">*</span></label>
+                                <input type="color" id="header_background_color" class="form-control" name="header_background_color" required>
+                            </div> 
+                                                     
+                        </div>
+
+                        <div class="card px-3 custom-shadow border-0" style="-webkit-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); -moz-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); box-shadow: inset 0px 0.5px 14px -8px rgba(0,0,0,0.75);">
+                            <h6 class="card-header p-0 mt-0">Bubble Animation</h6>
+                            
+                            <div class="custom-control custom-switch mt-2">
+                                <input type="checkbox" class="custom-control-input" name="enabled_animation" id="enabled_animation">
+                                <label class="custom-control-label" for="enabled_animation">Enabled Animation</label>
+                            </div>
+                            <div class="form-group mt-3">
+                                <label>Scroll Position <span class="text-danger">*</span></label>
+                                <input type="text" id="scroll_position_appearance" class="form-control" name="scroll_position_appearance" required>
+                            </div> 
+
+                        </div>
+
+                        <div class="card px-3 custom-shadow border-0" style="-webkit-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); -moz-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); box-shadow: inset 0px 0.5px 14px -8px rgba(0,0,0,0.75);">
+                            <h6 class="card-header p-0 mt-0">Start Chat Button</h6>
+                            
+                            <div class="form-group">
+                                <label for="button_corner_redius">Button Corner Radius</label>
+                                <input type="range" class="form-control-range" name="button_corner_redius" id="button_corner_redius">
+                            </div>
+
+                        </div>
+
+                        <div class="card px-3 custom-shadow border-0" style="-webkit-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); -moz-box-shadow: inset 0.5px 14px -8px rgba(0,0,0,0.75); box-shadow: inset 0px 0.5px 14px -8px rgba(0,0,0,0.75);">
+                            <h6 class="card-header p-0 mt-0">Custom CSS</h6>
+                                                        
+                            <div class="form-group mt-1">
+                                <textarea type="text" class="form-control" name="custom_css" rows="4" required></textarea>
+                           </div> 
+
+                        </div>
+
+                        
+
+                        
 
 
-
-
-                    <div class="tab-pane fade" id="appearance" role="tabpanel" aria-labelledby="appearance-tab">Appearance</div>
+                    </div>
+                
+                
+                
                 </div>
             </div>
         </div>
