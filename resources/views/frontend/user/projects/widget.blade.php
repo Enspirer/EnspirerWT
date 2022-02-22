4e4930c9b6a37529c2fee0c6a5bc2e1265337333
Achintha Dilshan <achintha.tallentor@gmail.com>
2022-02-22 15:54:27 +0530

    Modal and Navbar z index

--- resources/views/frontend/user/projects/widget.blade.php
+++ resources/views/frontend/user/projects/widget.blade.php
@@ -94,7 +94,7 @@
                                     </div>
                                     <div class="col-6">
                                         <div class="innwer-wrapper dotted-border">
-                                            <button class="btn-whatsapp"  data-toggle="modal" data-target=".bd-example-modal-lg">Create
+                                            <button type="button" class="btn-whatsapp"  data-bs-toggle="modal" data-bs-target="#exampleModal">Create
                                                 Widget</button>
                                         </div>
                                     </div>
@@ -104,12 +104,12 @@
 
                             <!-- Large modal -->
                             <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                                 Launch demo modal
-                            </button>
+                            </button> -->
 
                             <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
@@ -127,7 +127,35 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
+
+
+
+                            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+                            
 
 
 
