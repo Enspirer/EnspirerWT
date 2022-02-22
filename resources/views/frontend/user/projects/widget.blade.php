9fe633f63d02744b4d6f3dfd32b11ed1188ab38b
sanjayaharshana <sanjayaharasana@gmail.com>
2022-02-22 18:23:59 +0530

    Widget Function

--- resources/views/frontend/user/projects/widget.blade.php
+++ resources/views/frontend/user/projects/widget.blade.php
@@ -138,19 +138,177 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog" style="width: 1090px;height: 120px;max-width: 1010px;max-height: 1000px;">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+        <div class="card">
+            <div class="card-header" style="font-size: 20px;padding-left: 30px;padding-top: 10px;padding-bottom: 10px;">
+                Widget Explorer
+            </div>
+            <div class="card-body" style="height: 500px;overflow-y: scroll;">
+                <div class="block-1 mb-5" style="border-style: solid;border-width: 1px;border-color: gray;">
+                    <div class="row g-0">
+                        <div class="inner-wrapper p-4">
+                            <div class="col">
+                                <div class="row g-0 mb-3">
+                                    <div class="col">
+                                        <div class="header">
+                                            <span class="title">Whatsapp Chat</span>
+                                            <i class="bi bi-whatsapp"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row gx-4">
+                                    <div class="col-8">
+                                        <p class="body-text">Lorem ipsum dolor sit amet consectetur
+                                            adipisicing elit. Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit deserunt delectus quos
+                                            nostrum expedita omnis saepe, in labore quidem numquam quaerat.
+                                            Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit.
+                                        </p>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="button-block">
+                                            <button class="btn-whatsapp"></i>Create
+                                                Widget</button>
+                                            <i class="bi bi-whatsapp"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-0">
+                                    <div class="col">
+                                        <p class="footer-text">Give users an opportunity to contact you on
+                                            Whatsapp straight from your website.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="block-1 mb-5" style="border-style: solid;border-width: 1px;border-color: gray;">
+                    <div class="row g-0">
+                        <div class="inner-wrapper p-4">
+                            <div class="col">
+                                <div class="row g-0 mb-3">
+                                    <div class="col">
+                                        <div class="header">
+                                            <span class="title">Facebook Chat</span>
+                                            <i class="bi bi-facebook" style="color: darkblue"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row gx-4">
+                                    <div class="col-8">
+                                        <p class="body-text">Lorem ipsum dolor sit amet consectetur
+                                            adipisicing elit. Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit deserunt delectus quos
+                                            nostrum expedita omnis saepe, in labore quidem numquam quaerat.
+                                            Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit.
+                                        </p>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="button-block">
+                                            <button class="btn-whatsapp"></i>Create
+                                                Widget</button>
+                                            <i class="bi bi-facebook"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-0">
+                                    <div class="col">
+                                        <p class="footer-text">Give users an opportunity to contact you on
+                                            Whatsapp straight from your website.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="block-1 mb-5" style="border-style: solid;border-width: 1px;border-color: gray;">
+                    <div class="row g-0">
+                        <div class="inner-wrapper p-4">
+                            <div class="col">
+                                <div class="row g-0 mb-3">
+                                    <div class="col">
+                                        <div class="header">
+                                            <span class="title">Facebook Chat</span>
+                                            <i class="bi bi-facebook" style="color: darkblue"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row gx-4">
+                                    <div class="col-8">
+                                        <p class="body-text">Lorem ipsum dolor sit amet consectetur
+                                            adipisicing elit. Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit deserunt delectus quos
+                                            nostrum expedita omnis saepe, in labore quidem numquam quaerat.
+                                            Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit.
+                                        </p>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="button-block">
+                                            <button class="btn-whatsapp"></i>Create
+                                                Widget</button>
+                                            <i class="bi bi-facebook"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-0">
+                                    <div class="col">
+                                        <p class="footer-text">Give users an opportunity to contact you on
+                                            Whatsapp straight from your website.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="block-1 mb-5" style="border-style: solid;border-width: 1px;border-color: gray;">
+                    <div class="row g-0">
+                        <div class="inner-wrapper p-4">
+                            <div class="col">
+                                <div class="row g-0 mb-3">
+                                    <div class="col">
+                                        <div class="header">
+                                            <span class="title">Facebook Chat</span>
+                                            <i class="bi bi-facebook" style="color: darkblue"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row gx-4">
+                                    <div class="col-8">
+                                        <p class="body-text">Lorem ipsum dolor sit amet consectetur
+                                            adipisicing elit. Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit deserunt delectus quos
+                                            nostrum expedita omnis saepe, in labore quidem numquam quaerat.
+                                            Mollitia sit maxime nam sapiente illum,
+                                            consequuntur aperiam quia reprehenderit.
+                                        </p>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="button-block">
+                                            <button class="btn-whatsapp"></i>Create
+                                                Widget</button>
+                                            <i class="bi bi-facebook"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row g-0">
+                                    <div class="col">
+                                        <p class="footer-text">Give users an opportunity to contact you on
+                                            Whatsapp straight from your website.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     </div>
   </div>
 </div>
