9d1daa7dd9f2581280f31f3a794a18b0af248b6f
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-02-28 15:45:03 +0530

    pages dynamic

--- app/Http/Controllers/Frontend/IMSController.php
+++ app/Http/Controllers/Frontend/IMSController.php
@@ -65,7 +65,7 @@ class IMSController extends Controller
         // dd($id);
         $widget = Widgets::where('id',$id)->first();
         $project = Projects::where('id',$widget->project_id)->first();           
-        $ims_client = ImsClients::where('widget_id',$widget->id)->get();
+        $ims_client = ImsClients::where('widget_id',$widget->id)->where('assign_by','!=', null)->get();
         // dd($ims_client);
     
         return view('frontend.user.projects.ims.user_widget_ims_assigned',[
@@ -81,7 +81,7 @@ class IMSController extends Controller
         // dd($id);
         $widget = Widgets::where('id',$id)->first();
         $project = Projects::where('id',$widget->project_id)->first();           
-        $ims_client = ImsClients::where('widget_id',$widget->id)->get();
+        $ims_client = ImsClients::where('widget_id',$widget->id)->where('assign_by', null)->get();
         // dd($ims_client);
     
         return view('frontend.user.projects.ims.user_widget_ims_unassigned',[
