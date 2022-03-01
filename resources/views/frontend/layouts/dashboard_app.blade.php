978c6fff6f2f15f213ff0aefe6583dffcae19442
Achintha Dilshan <achintha.tallentor@gmail.com>
2022-03-01 10:04:34 +0530

    IMS Dashboard button

--- resources/views/frontend/layouts/dashboard_app.blade.php
+++ resources/views/frontend/layouts/dashboard_app.blade.php
@@ -55,6 +55,7 @@
         <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/js/splide.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.lordicon.com/lusqsztk.js"></script>
         <script src="{{url('js/script.js')}}"></script>
         <script src="{{url('css/jquery-clockpicker.min.js')}}"></script>
 
