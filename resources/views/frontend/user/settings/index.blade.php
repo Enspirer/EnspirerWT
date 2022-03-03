31240a7ca00001deca92d03a27fa179226042463
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-03-03 13:09:31 +0530

    settings image

--- resources/views/frontend/user/settings/index.blade.php
+++ resources/views/frontend/user/settings/index.blade.php
@@ -73,7 +73,7 @@
                                         <div class="row g-0">
                                             <div class="col-4">
                                                 <div class="profile-pic">
-                                                    <img src="{{url('images/profile.png')}}" alt="">
+                                                    <img src="{{ auth()->user()->picture }}" alt="">
                                                     <i class="bi bi-pencil-fill"></i>
                                                 </div>
                                             </div>
