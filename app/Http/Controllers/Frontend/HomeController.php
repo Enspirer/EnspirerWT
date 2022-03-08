ec4ae1a8368f8463ad76216e7d6fc1c82298ce98
sanjayaharshana <sanjayaharasana@gmail.com>
2022-03-08 09:52:28 +0530

    Home Controller PHP Functions

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -42,7 +42,8 @@ class HomeController extends Controller
         $incom =  urlencode($request->usermessage);
 
 
-        return redirect()->to('https://wa.me/'.$phone_number.'/?text='.$incom);
+        return redirect()->to('https://web.whatsapp.com/send?phone='.$phone_number.'&text='.$incom);
+//        return redirect()->to('https://wa.me/'.$phone_number.'/?text='.$incom);
 
     }
 
@@ -87,7 +88,7 @@ class HomeController extends Controller
 
         if($request->contact_via == 'Whatsapp'){
             if ($getSettings[0]->whatsapp_details[0] !=null) {
-                return redirect()->to('https://wa.me/'.$phone_number.'/?text='.$incom);
+                return redirect()->to('https://web.whatsapp.com/send?phone='.$phone_number.'&text='.$incom);
             }
         }else if ($request->contact_via == 'Messenger'){
             if ($getSettings[0]->fb_details[0] !=null) {
