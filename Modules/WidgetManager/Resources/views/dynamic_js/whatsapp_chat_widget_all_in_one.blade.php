88eb7bd27b30fda4f957c137acaf4158a422a0fa
sanjayaharshana <sanjayaharasana@gmail.com>
2022-03-09 11:37:46 +0530

    WhasApp function APIS

--- Modules/WidgetManager/Resources/views/dynamic_js/whatsapp_chat_widget_all_in_one.blade.php
+++ Modules/WidgetManager/Resources/views/dynamic_js/whatsapp_chat_widget_all_in_one.blade.php
@@ -690,3 +690,18 @@ function allin1tawktoiframe() {
 function allin1closetawktoiframe() {
     document.querySelector('.allin1ifameblock').classList.remove("allin1view");
 }
+
+function random(min,max){
+ return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var initial = random(500, 2000);
+var count = initial;
+
+ setInterval(function() {
+ var variation = random(-5,5);
+
+ count += variation
+ console.log('You currently have ' + count + ' visitors')
+
+ }, 2000)
