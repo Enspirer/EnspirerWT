603b05952ec7f079946bc0bde311f853047cd599
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-03-15 19:30:16 +0530

    visitor statistic

--- app/Http/Controllers/Frontend/User/VisitorStatisticsController.php
+++ app/Http/Controllers/Frontend/User/VisitorStatisticsController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Widgets;
 use App\Models\Projects;
+use App\Models\VisitorCount;
 use Modules\WidgetManager\Entities\ImsClients;
 use DB;
+use Carbon\Carbon;
 
 class VisitorStatisticsController extends Controller
 {
@@ -15,18 +17,29 @@ class VisitorStatisticsController extends Controller
     public function index($id)
     {
         // dd($id);
+        $date = Carbon::now()->subDays(7);
+
+        $ims_clienrt = ImsClients::where('project_id',$id)->where('created_at', '>=', Carbon::now()->subMonth())
+                            ->groupBy('date')
+        ->orderBy('date', 'DESC')
+        ->get(array(
+            DB::raw('COUNT(status = "Deal close successfully") as "responded_count" '),
+            DB::raw('Date(created_at) as date'),
+            DB::raw('COUNT(*) as "count"'),
+        ));
+
+        //dd($id);
         $project = Projects::where('id',$id)->first();           
-
-
-        // $widget = Widgets::where('id',$project)->first();
-
-        // $ims_client = ImsClients::where('project_id',$project->id)->orderBy('id','desc')->get();
+        $ims_client = ImsClients::where('project_id',$project->id)->get();
         // dd($ims_client);
+
+        $visitors = VisitorCount::where('project_id',$id)->get();
     
         return view('frontend.user.projects.ims.user_widget_visitor_statistics',[
+            'chart_data' => $ims_clienrt,
             'project' => $project,
-            // 'widget' => $widget,
-            // 'ims_client' => $ims_client
+            'ims_client' => $ims_client,
+            'visitors' => $visitors
         ]);
         
     }
--- resources/views/frontend/user/projects/ims/user_widget_visitor_statistics.blade.php
+++ resources/views/frontend/user/projects/ims/user_widget_visitor_statistics.blade.php
@@ -20,7 +20,7 @@
                                         <ul class="breadcrumb">
                                             
                                             <li class="breadcrumb-item">
-                                                <a class="breadcrumb-link">Visitor Statistics</a>
+                                                <!-- <a class="breadcrumb-link">Visitor Statistics</a> -->
                                             </li>
                                             <!-- <li class="breadcrumb-item">
                                                 <a href="#" class="breadcrumb-link breadcrumb-arrow"><i
@@ -37,7 +37,60 @@
                                         <section class="coversation-section ims__panel" id="ims__conversation">
                                             <div class="inbox--section ims__panel" id="ims__inbox">
                                               
-                                                    
+                                            <div class="row g-0">
+                                                <div class="col-6">
+                                                    <div class="card p-4" style="border-radius:20px;">
+                                                        <div class="content">
+                                                        
+                                                            <div class="chart-section">
+                                                                <div id="analyticsChart" style="width: 100%;height: 350px;"></div>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-6">
+                                                    <div class="card p-4 ms-3" style="border-radius:20px;">
+                                                        <div class="content"> 
+
+                                                            <div class="caption"><i class="bi bi-broadcast"></i> Live Visitors</div>
+                                                            <h4 class="count">{{\App\Models\VisitorCount::where('project_id',$project->id)->count()}}</h4>
+                                
+                                                            @if(count($visitors) != 0)                                                               
+                                                                <div class="ims__data-table">
+                                                                    <table class="table table-borderless">
+                                                                        <thead>
+                                                                            <tr class="data-row">
+                                                                               
+                                                                                <th class="data-title">Project ID</th>
+                                                                                <th class="data-title">Widget ID</th>
+                                                                                <th class="data-title">IP Address</th>
+                                                                            </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                            @foreach($visitors as $key => $visitor)
+                                                                                <tr class="data-row">
+                                                                                                                                                                        
+                                                                                    <td class="data--name data-cell">
+                                                                                        <div class="text">{{$visitor->project_id}}</div>
+                                                                                    </td>
+                                                                                    <td class="data--date data-cell">
+                                                                                        <div class="text">{{$visitor->widget_id}}</div>
+                                                                                    </td>
+                                                                                    <td class="data--date data-cell">
+                                                                                        <div class="text">{{$visitor->ip_address}}</div>
+                                                                                    </td>                                                                                    
+                                                                                </tr>
+                                                                            @endforeach
+                                                                            
+                                                                        </tbody>
+                                                                    </table>
+                                                                </div>
+                                                            @endif
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                             
                                                 
                                             </div>        
@@ -61,6 +114,113 @@
 
 @push('after-scripts')
 
-   
+<script type="text/javascript">
+
+    var chartDom = document.getElementById('analyticsChart');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    option = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+        type: 'cross',
+        crossStyle: {
+            color: '#999'
+        }
+        }
+    },
+    color: [
+        '#44BC8E',
+        '#1B3D6B',
+        '#0d6efd',
+    ],
+    legend: {
+        data: ['Respond Leeds', 'Total Leeds',  'Rates']
+    },
+    xAxis: [
+        {
+        type: 'category',
+        data: [
+            @foreach($chart_data as $chartFunc)
+            '{{$chartFunc->date}}',
+            @endforeach
+        ],
+        axisPointer: {
+            type: 'shadow'
+        }
+        }
+    ],
+    yAxis: [
+        {
+        type: 'value',
+        name: 'Total Leeds',
+        min: 0,
+        interval: 50,
+        axisLabel: {
+            formatter: '{value}'
+        }
+        },
+        {
+        type: 'value',
+        name: 'Respond Leeds',
+        min: 0,
+        interval: 5,
+        axisLabel: {
+            formatter: '{value}'
+        }
+        }
+    ],
+    series: [
+        {
+        name: 'Respond Leeds',
+        type: 'bar',
+        tooltip: {
+            valueFormatter: function (value) {
+            return value + '';
+            }
+        },
+        data: [
+            @foreach($chart_data as $chartFunc)
+            {{$chartFunc->responded_count}},
+            @endforeach
+        ]
+        },
+        {
+        name: 'Total Leeds',
+        type: 'bar',
+        tooltip: {
+            valueFormatter: function (value) {
+            return value + '';
+            }
+        },
+        data: [
+            @foreach($chart_data as $chartFunc)
+            {{$chartFunc->count}},
+            @endforeach
+        ]
+        },
+        {
+        name: 'Rates',
+        type: 'line',
+        smooth: true,
+        yAxisIndex: 1,
+        tooltip: {
+            valueFormatter: function (value) {
+            return value + '';
+            }
+        },
+        data: [
+            @foreach($chart_data as $chartFunc)
+            {{$chartFunc->count}},
+            @endforeach
+        ]
+        }
+    ]
+    };
+
+    option && myChart.setOption(option);
+
+    </script>
 
 @endpush
