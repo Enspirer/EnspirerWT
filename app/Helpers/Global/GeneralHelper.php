78b15c56f357b4ac8a3bff7ae1d556ffbc7d76ca
sanjayaharshana <sanjayaharasana@gmail.com>
2022-03-19 01:40:12 +0530

    General Helper Function

--- app/Helpers/Global/GeneralHelper.php
+++ app/Helpers/Global/GeneralHelper.php
@@ -190,7 +190,17 @@ if (! function_exists('push_notification')) {
     }
 }
 
+if (! function_exists('calcGrowth')) {
+    function calcGrowth($current, $previous)
+    {
+        if ($previous == 0 || $previous == null || $current == 0) {
+            return 0;
+        }
 
+        return $result = (($current - $previous) / $previous * 100);
+    }
+
+}
 
 
 
