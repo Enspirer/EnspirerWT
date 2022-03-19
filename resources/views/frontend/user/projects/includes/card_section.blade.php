467718cb25260d70ab8c7e4a81125a63e4592eae
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-03-19 10:22:31 +0530

    card section changes

--- resources/views/frontend/user/projects/includes/card_section.blade.php
+++ resources/views/frontend/user/projects/includes/card_section.blade.php
@@ -20,7 +20,7 @@
                 <div class="tab-icon">
                     <i class="bi bi-activity"></i>
                 </div>
-                <div class="tab-title">Analytics</div>
+                <div class="tab-title">Analytics Center</div>
                 <div class="tab-info">
                     <span class="strong-text">03</span>
                     Visitors
@@ -65,11 +65,20 @@
 
 <div class="row g-0 mb-5">
     @if(Request::segment(3)=='widget')
-        <a href="{{ route('frontend.user.user_widget.ims_dashboard', $project_id) }}" target="_blank" class="ims-dashboard-btn">
-            <lord-icon src="https://cdn.lordicon.com/yyecauzv.json" trigger="loop" style="width:65px;height:65px">
-            </lord-icon>
-            IMS Dashboard
-        </a>
+
+        @if(App\Models\Widgets::where('project_id',$project_id)->where('widget_type','Whatsapp Chat')->first() == null && App\Models\Widgets::where('project_id',$project_id)->where('widget_type','All-in-One Chat')->first() == null)
+            <a data-bs-toggle="modal" data-bs-target="#widgetdisableModal" class="ims-dashboard-btn">
+                <lord-icon src="https://cdn.lordicon.com/yyecauzv.json" trigger="loop" style="width:65px;height:65px">
+                </lord-icon>
+                IMS Dashboard
+            </a>
+        @else
+            <a href="{{ route('frontend.user.user_widget.ims_dashboard', $project_id) }}" target="_blank" class="ims-dashboard-btn">
+                <lord-icon src="https://cdn.lordicon.com/yyecauzv.json" trigger="loop" style="width:65px;height:65px">
+                </lord-icon>
+                IMS Dashboard
+            </a>
+        @endif
 
 
         <a href="{{ route('frontend.user.project_settings', $project_id) }}" class="ims-dashboard-btn ms-5">
@@ -78,16 +87,26 @@
     @endif
 
     @if(Request::segment(3)=='analytics')
-        <a href="{{ route('frontend.user.user_widget.analytics', $project_id) }}" target="_blank" class="ims-dashboard-btn">
+        <!-- <a href="{{ route('frontend.user.user_widget.analytics', $project_id) }}" target="_blank" class="ims-dashboard-btn">
             <i class="bi bi-clipboard2-pulse"></i>
             Analytics
-        </a>
+        </a> -->
 
-        <a href="{{ route('frontend.user.user_widget.analytics', $project_id) }}" target="_blank" class="ims-dashboard-btn ms-5">
-            <i class="bi bi-clipboard2-pulse"></i>
-            Statistics
-        </a>
+        @if(App\Models\Widgets::where('project_id',$project_id)->where('widget_type','Analytics')->first() == null)
+            <a data-bs-toggle="modal" data-bs-target="#analyticsdisableModal" class="ims-dashboard-btn">
+                <i class="bi bi-clipboard2-pulse"></i>
+                Statistics
+            </a>
 
+        @else
+            <a href="{{ route('frontend.user.user_widget.analytics', $project_id) }}" target="_blank" class="ims-dashboard-btn">
+                <i class="bi bi-clipboard2-pulse"></i>
+                Statistics
+            </a>
+
+        @endif
+
+     
         <a href="{{ route('frontend.user.user_widget.analytics', $project_id) }}" target="_blank" class="ims-dashboard-btn ms-5">
             <i class="bi bi-clipboard2-pulse"></i>
             Heatmaps
@@ -100,4 +119,93 @@
     @endif
 
 
+</div>
+
+
+<div class="modal fade" id="widgetdisableModal" tabindex="-1" aria-labelledby="createWidgetModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="modal-title" id="createWidgetModalLabel">Active Widget</h5>
+                    <button type="button" class="btn-close-modal" data-bs-dismiss="modal"
+                        aria-label="Close">
+                        <i class="bi bi-x-lg"></i>
+                    </button>
+                </div>
+                <div class="card-body">
+                    <div class="block-1">
+                        <div class="row g-0">
+                                                   
+                                <div class="inner-wrapper">
+                                    <div class="col">
+                                        <div class="row g-0 align-items-center justify-content-between">
+                                            <div class="col-12">
+                                                <div class="header">
+                                                    <span class="title">In order to view Whatsapp Widget and All-in-one Widget click here.</span>
+                                                
+                                                </div>
+                                               
+                                                <button type="button" class="btn-whatsapp" data-bs-toggle="modal"
+                                                   data-bs-target="#createWidgetModal">Create Widget</button>
+                                              
+                                                
+                                              
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                                            
+                                </div>
+                                                 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="analyticsdisableModal" tabindex="-1" aria-labelledby="createWidgetModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="modal-title" id="createWidgetModalLabel">Active Analytics</h5>
+                    <button type="button" class="btn-close-modal" data-bs-dismiss="modal"
+                        aria-label="Close">
+                        <i class="bi bi-x-lg"></i>
+                    </button>
+                </div>
+                <div class="card-body">
+                    <div class="block-1">
+                        <div class="row g-0">
+                                                   
+                                <div class="inner-wrapper">
+                                    <div class="col">
+                                        <div class="row g-0 align-items-center justify-content-between">
+                                            <div class="col-12">
+                                                <div class="header">
+                                                    <span class="title">In order to view statistics, heatmaps, session recordings click here.</span>
+                                                
+                                                </div>
+                                               
+                                                <button type="button" class="btn-whatsapp" data-bs-toggle="modal"
+                                                   data-bs-target="#createAnalyticsModal">Create Analytics</button>
+                                              
+                                                
+                                              
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                                            
+                                </div>
+                                                 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
