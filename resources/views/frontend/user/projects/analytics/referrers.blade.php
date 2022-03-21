1e40526f7f74fdb2cb3ec09f29a5074315d787a3
sanjayaharshana <sanjayaharasana@gmail.com>
2022-03-21 23:30:31 +0530

    Refeence Function Stack

--- resources/views/frontend/user/projects/analytics/referrers.blade.php
+++ resources/views/frontend/user/projects/analytics/referrers.blade.php
@@ -90,8 +90,14 @@
                                                         <div class="row-subtitle">
                                                             <div class="subtitle">Total</div>
                                                             <div class="status">
-                                                                <div class="total-count">{{ number_format($referrer->count, 0, __('.'), __(',')) }}</div>
-                                                                <div class="total-precentage">{{ number_format((($referrer->count / $total->count) * 100), 1, __('.'), __(',')) }}%</div>
+                                                                @if( $total->count == null)
+                                                                    <div class="total-count">0</div>
+                                                                    <div class="total-precentage">0%</div>
+                                                                @else
+                                                                    <div class="total-count">{{ number_format($referrer->count, 0, __('.'), __(',')) }}</div>
+                                                                    <div class="total-precentage">{{ number_format((($referrer->count / $total->count) * 100), 1, __('.'), __(',')) }}%</div>
+                                                                @endif
+
                                                             </div>
                                                         </div>
                                                         @foreach($referrers as $referrer)
