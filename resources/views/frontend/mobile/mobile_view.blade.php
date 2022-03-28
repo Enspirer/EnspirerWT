830ff2664f0cf38614dd960191eb07613d45864c
Achintha Dilshan <achintha.tallentor@gmail.com>
2022-03-29 05:02:10 +0530

    Mobile Nav

--- resources/views/frontend/mobile/mobile_view.blade.php
+++ resources/views/frontend/mobile/mobile_view.blade.php
@@ -14,7 +14,7 @@
         <div class="top-nav">
             <div class="inner-wrapper">
                 <div class="navs">
-                    <img src="{{url('images/mobile/home/hamburger.png')}}" alt="">
+                    <img src="{{url('images/mobile/home/hamburger.png')}}" alt="" data-bs-toggle="modal" data-bs-target="#mobileNav">
                 </div>
                 <div class="profile">
                     <img src="{{url('images/mobile/home/profile-picture.png')}}" alt="">
@@ -229,7 +229,7 @@
     </div>
 </section>
 
-<!-- Modal -->
+<!-- Modal: Add Button -->
 <div class="modal fade addProperty-model mobile-model" id="addProperty" tabindex="-1" aria-labelledby="addPropertyLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -308,7 +308,7 @@
   </div>
 </div>
 
-<!-- Modal -->
+<!-- Modal: Add Property -->
 <div class="modal fade addPropertyInput-model mobile-model" id="addPropertyInput" tabindex="-1" aria-labelledby="addPropertyInputLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -335,6 +335,74 @@
       <div class="modal-footer">
         <a href="#" type="button" class="btn-mobile btn-project-submit">Submit</a>
         <a href="#" type="button" class="btn-mobile btn-cancel" data-bs-dismiss="modal">Close</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal: Mobile Nav -->
+<div class="modal fade mobile-nav" id="mobileNav" tabindex="-1" aria-labelledby="mobileNavLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="profile">
+            <img src="{{url('images/mobile/home/profile-picture.png')}}" alt="">
+            <div class="text-block">
+                <div class="name">Amal Perera</div>
+                <div class="active-status active">Active Status</div>
+            </div>
+        </div>
+        <button type="button" class="bi bi-x-lg" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link active" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="bi bi-house-door-fill"></i>
+                    <div class="text">Home</div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="bi bi-calendar2"></i>
+                    <div class="text">Day Views</div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="bi bi-bar-chart-fill"></i>
+                    <div class="text">Analytics</div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="bi bi-bell-fill"></i>
+                    <div class="text">Notifications</div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="bi bi-person-fill"></i>
+                    <div class="text">Account</div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="bi bi-sliders2-vertical"></i>
+                    <div class="text">Settings</div>
+                </a>
+            </li>
+        </ul>
+        <div class="image-block">
+            <div class="rectangle"></div>
+            <img src="{{url('images/mobile/home/nav-image.png')}}" alt="">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a href="#" class="btn-mobile btn-signout" data-bs-dismiss="modal" aria-label="Close">
+            <i class="bi bi-box-arrow-right"></i>
+            <div class="text">Sign Out</div>
+        </a>
       </div>
     </div>
   </div>
