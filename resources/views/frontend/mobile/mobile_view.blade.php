ff68389d73107f561afd11d70151e850445fcb90
sanjayaharshana <sanjayaharasana@gmail.com>
2022-03-31 16:14:06 +0530

    Mobile ISO Codes

--- resources/views/frontend/mobile/mobile_view.blade.php
+++ resources/views/frontend/mobile/mobile_view.blade.php
@@ -121,7 +121,7 @@
                                 <tr>
                                     <td class="country-flag">
 
-                                        <img src="{{url('https://flagicons.lipis.dev/flags/4x3/'.$visitors->iso_code .'.svg')}}" alt="">
+                                        <img src="{{url('https://flagicons.lipis.dev/flags/4x3/'.strtolower($visitors->iso_code) .'.svg')}}" alt="">
                                     </td>
                                     <td class="country-name">{{$visitors->ip_address}}</td>
                                     <td class="active-status">
