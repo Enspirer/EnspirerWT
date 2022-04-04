370c86cad0d44f9d45bf645aec960dd8ca3293b3
Achintha Dilshan <achintha.tallentor@gmail.com>
2022-04-04 13:08:34 +0530

    not found page

--- resources/views/frontend/includes/not_found.blade.php
+++ resources/views/frontend/includes/not_found.blade.php
@@ -1,8 +1,14 @@
-<div class="row" style="margin:50px 0 50px 0">
+<!-- <div class="row" style="margin:50px 0 50px 0">
     <div class="container">
         <div style="text-align: center">
             <div style="background-image: url('{{url('img/no_data.png')}}');height: 300px;background-position: center;background-repeat: no-repeat;background-size: contain;"></div>
             <h3>{{$not_found_title}}</h3>
         </div>
     </div>
+</div> -->
+
+
+
+<div class="not-found-window">
+    <img src="{{url('images/dashboard/ims_pro/chat-window.png')}}" alt="">
 </div>
