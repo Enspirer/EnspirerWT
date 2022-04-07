68cc783f3bd127ce40ab210856f0d86746774d9a
Achintha Dilshan <achintha.tallentor@gmail.com>
2022-04-07 05:42:29 +0530

    Package blocks

--- resources/views/frontend/user/projects/includes/card_section.blade.php
+++ resources/views/frontend/user/projects/includes/card_section.blade.php
@@ -1,4 +1,4 @@
-<div class="section-cards" id="sectionCards">
+<!-- <div class="section-cards" id="sectionCards">
     <div class="cards">
         <div class="card {{Request::segment(3)=='widget' ? 'active' :null }}">
             <a href="{{route('frontend.user.project.chat',$project_id)}}" class="card-link">
@@ -178,8 +178,238 @@
             </div>
         </div>
     </div>
+</div> -->
+
+<div class="section-cards" id="sectionCards">
+    <div class="cards">
+        <div class="card {{Request::segment(3)=='widget' ? 'active' :null }}">
+            <a href="{{route('frontend.user.project.chat',$project_id)}}" class="card-link">
+                <i class="bi bi-ui-checks-grid"></i>
+                <div class="text">Widget</div>
+            </a>
+        </div>
+        <div class="card {{Request::segment(3)=='widget_plus' ? 'active' :null }}">
+            <a href="{{route('frontend.user.project.widget_plus',$project_id)}}" class="card-link">
+                <i class="bi bi-ui-checks-grid"></i>
+                <div class="text">Widget Plus</div>
+            </a>
+        </div>
+        <div class="card {{Request::segment(3)=='optimizer' ? 'active' :null }}">
+            <a href="{{route('frontend.user.project.optimizer',$project_id)}}" class="card-link">
+                <i class="bi bi-ui-checks-grid"></i>
+                <div class="text">Optimizer</div>
+            </a>
+        </div>
+        <div class="card {{Request::segment(3)=='optimizer_plus' ? 'active' :null }}">
+            <a href="{{route('frontend.user.project.optimizer',$project_id)}}" class="card-link">
+                <i class="bi bi-ui-checks-grid"></i>
+                <div class="text">Optimizer Plus</div>
+            </a>
+        </div>
+    </div>
 </div>
 
+<div class="subcription-block">
+    <div class="inner-wrapper">
+        <img src="{{url('images/dashboard/subscriptions/whatsapp-water-mark.png')}}" alt="" class="water-mark">
+        <div class="widget-block">
+            <div class="title">WhatsApp Widget</div>
+            <img src="{{url('images/dashboard/subscriptions/whatsapp.png')}}" alt="" class="widget-img">
+        </div>
+        <div class="separator">
+            <i class="bi bi-plus-lg"></i>
+        </div>
+        <div class="widget-block">
+            <div class="title">IMS <span class="pro-cat blue">Lite</span></div>
+            <img src="{{url('images/dashboard/subscriptions/ims-lite.png')}}" alt="" class="widget-img">
+        </div>
+        <a href="#" class="sub-btn">
+            <i class="bi bi-plus"></i>
+            <div class="text">Activate</div>
+        </a>
+    </div>
+</div>
+
+<div class="subcription-block">
+    <div class="inner-wrapper">
+        <img src="{{url('images/dashboard/subscriptions/messenger-water-mark.png')}}" alt="" class="water-mark">
+        <div class="widget-block">
+            <div class="title">All in one Widget</div>
+            <img src="{{url('images/dashboard/subscriptions/all-in-one.png')}}" alt="" class="widget-img">
+        </div>
+        <div class="separator">
+            <i class="bi bi-plus-lg"></i>
+        </div>
+        <div class="widget-block">
+            <div class="title">IMS <span class="pro-cat blue">Lite</span></div>
+            <img src="{{url('images/dashboard/subscriptions/ims-lite.png')}}" alt="" class="widget-img">
+        </div>
+        <a href="#" class="sub-btn">
+            <i class="bi bi-plus"></i>
+            <div class="text">Activate</div>
+        </a>
+    </div>
+</div>
+
+<div class="subcription-block">
+    <div class="inner-wrapper">
+        <img src="{{url('images/dashboard/subscriptions/whatsapp-water-mark.png')}}" alt="" class="water-mark">
+        <div class="widget-block">
+            <div class="title">WhatsApp Widget</div>
+            <img src="{{url('images/dashboard/subscriptions/whatsapp.png')}}" alt="" class="widget-img">
+        </div>
+        <div class="separator">
+            <i class="bi bi-plus-lg"></i>
+        </div>
+        <div class="widget-block">
+            <div class="title">IMS <span class="pro-cat red">Pro</span></div>
+            <img src="{{url('images/dashboard/subscriptions/ims-pro.png')}}" alt="" class="widget-img">
+        </div>
+        <a href="#" class="sub-btn">
+            <i class="bi bi-plus"></i>
+            <div class="text">Activate</div>
+        </a>
+    </div>
+</div>
+
+<div class="subcription-block">
+    <div class="inner-wrapper">
+        <img src="{{url('images/dashboard/subscriptions/messenger-water-mark.png')}}" alt="" class="water-mark">
+        <div class="widget-block">
+            <div class="title">All in one Widget</div>
+            <img src="{{url('images/dashboard/subscriptions/all-in-one.png')}}" alt="" class="widget-img">
+        </div>
+        <div class="separator">
+            <i class="bi bi-plus-lg"></i>
+        </div>
+        <div class="widget-block">
+            <div class="title">IMS <span class="pro-cat red">Pro</span></div>
+            <img src="{{url('images/dashboard/subscriptions/ims-pro.png')}}" alt="" class="widget-img">
+        </div>
+        <a href="#" class="sub-btn">
+            <i class="bi bi-plus"></i>
+            <div class="text">Activate</div>
+        </a>
+    </div>
+</div>
+
+<div class="subcription-block">
+    <div class="content-block">
+        <div class="widget-block">
+            <img src="{{url('images/dashboard/subscriptions/ims-pro.png')}}" alt="" class="widget-img">
+            <div class="content">
+                <div class="title">Desktop</div>
+                <ul class="options">
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Realtime View + Invites</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Analytics Center</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">SEO Reports</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi red bi-x-circle-fill"></i>
+                        <div class="text">Session Recording</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi red bi-x-circle-fill"></i>
+                        <div class="text">Heatmaps</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi red bi-x-circle-fill"></i>
+                        <div class="text">Security</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="widget-block">
+            <img src="{{url('images/dashboard/subscriptions/mobile-app.png')}}" alt="" class="widget-img">
+            <div class="content">
+                <div class="title">Mobile</div>
+                <ul class="options">
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Realtime View</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Visitor Invite</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="button-block">
+        <a href="#" class="sub-btn">
+            <i class="bi bi-plus"></i>
+            <div class="text">Activate</div>
+        </a>
+    </div>
+</div>
+
+<div class="subcription-block">
+    <div class="content-block">
+        <div class="widget-block">
+            <img src="{{url('images/dashboard/subscriptions/ims-pro.png')}}" alt="" class="widget-img">
+            <div class="content">
+                <div class="title">Desktop</div>
+                <ul class="options">
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Realtime View + Invites</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Analytics Center</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">SEO Reports</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Session Recording</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Heatmaps</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Security</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="widget-block">
+            <img src="{{url('images/dashboard/subscriptions/mobile-app.png')}}" alt="" class="widget-img">
+            <div class="content">
+                <div class="title">Mobile</div>
+                <ul class="options">
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Realtime View</div>
+                    </li>
+                    <li class="list-item">
+                        <i class="bi green bi-check-circle-fill"></i>
+                        <div class="text">Visitor Invite</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="button-block">
+        <a href="#" class="sub-btn">
+            <i class="bi bi-plus"></i>
+            <div class="text">Activate</div>
+        </a>
+    </div>
+</div>
 
 <div class="modal fade dashboard-modal action-modal" id="widgetdisableModal" tabindex="-1" aria-labelledby="createWidgetModalLabel"
     aria-hidden="true">
