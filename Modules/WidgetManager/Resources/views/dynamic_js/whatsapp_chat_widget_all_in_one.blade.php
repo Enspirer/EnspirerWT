ee4cc0fa597fcc0670f93c103936770c1196ce53
sanjayaharshana <sanjayaharasana@gmail.com>
2022-04-12 16:49:26 +0530

    Whatsapp Chat Widget All In One

--- Modules/WidgetManager/Resources/views/dynamic_js/whatsapp_chat_widget_all_in_one.blade.php
+++ Modules/WidgetManager/Resources/views/dynamic_js/whatsapp_chat_widget_all_in_one.blade.php
@@ -970,13 +970,14 @@ function myTimer() {
  var xhttp = new XMLHttpRequest();
  xhttp.onreadystatechange = function() {
    if (this.readyState == 4 && this.status == 200) {
-       console.log(this.responseText);
+       const oretesx = JSON.parse(xhttp.responseText);
+       console.log(oretesx);
    }
  };
  xhttp.open("POST", "{{url('api/log_checker')}}", true);
  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
  xhttp.send("widget_id=" + widget_id + "&current_url=" + current_url);
- console.log(xhttp.responseText);
+
 }
 
 
