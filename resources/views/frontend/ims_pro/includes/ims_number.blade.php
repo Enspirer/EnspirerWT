b57a02da3fb4fe4a45c8c0e5768de2a75c1dcb6f
sanjayaharshana <sanjayaharasana@gmail.com>
2022-04-12 10:02:25 +0530

    Content Comments

--- resources/views/frontend/ims_pro/includes/ims_number.blade.php
+++ resources/views/frontend/ims_pro/includes/ims_number.blade.php
@@ -1,23 +1,23 @@
-    <div class="row g-0">
-        <div class="welcome-block section-scan">
-            <div class="info-block">
-                <h2 class="title">Good Morning, Kasun !</h2>
-                <h2 class="sub-title">How to connect your own number ?</h2>
-                <div class="text">You must send the WhatsApp Message from your registered number +94775037754</div>
-                <div class="text">Please use this keyword: #5bMXv to receive messages from other numbers</div>
-            </div>
-            <div class="steps-block">
-                <img src="{{url('images/dashboard/ims_pro/section-scan.png')}}" alt="" class="img-main">
-            </div>
-        </div>
-    </div>
+    {{--<div class="row g-0">--}}
+        {{--<div class="welcome-block section-scan">--}}
+            {{--<div class="info-block">--}}
+                {{--<h2 class="title">Good Morning, Kasun !</h2>--}}
+                {{--<h2 class="sub-title">How to connect your own number ?</h2>--}}
+                {{--<div class="text">You must send the WhatsApp Message from your registered number +94775037754</div>--}}
+                {{--<div class="text">Please use this keyword: #5bMXv to receive messages from other numbers</div>--}}
+            {{--</div>--}}
+            {{--<div class="steps-block">--}}
+                {{--<img src="{{url('images/dashboard/ims_pro/section-scan.png')}}" alt="" class="img-main">--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
-    <div class="row g-o">
-        <div class="section-setupProcess">
-            <div class="activation-info">Estimated Activation end date : Apr 14</div>
-            <div class="title">Give Us to 24 hours to setup</div>
-            <img src="{{url('images/dashboard/ims_pro/setup-process.png')}}" alt="" class="process-img">
-            <div class="text">Thank you for your request to activate your own number, this process may take up to 24 hours to set up. our team will notify when the account is active.</div>
-        </div>
-    </div>
+    {{--<div class="row g-o">--}}
+        {{--<div class="section-setupProcess">--}}
+            {{--<div class="activation-info">Estimated Activation end date : Apr 14</div>--}}
+            {{--<div class="title">Give Us to 24 hours to setup</div>--}}
+            {{--<img src="{{url('images/dashboard/ims_pro/setup-process.png')}}" alt="" class="process-img">--}}
+            {{--<div class="text">Thank you for your request to activate your own number, this process may take up to 24 hours to set up. our team will notify when the account is active.</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
