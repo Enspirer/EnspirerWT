b046adddc6c5cd52d70a5d29130439e9f8a81f03
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-05-11 03:41:19 +0530

    IMX Index Funcio

--- resources/views/frontend/ims_pro/includes/ims_index.blade.php
+++ resources/views/frontend/ims_pro/includes/ims_index.blade.php
@@ -147,7 +147,7 @@
                                                                                                     <div class="status-block">
                                                                                                         @if(get_contact_info($ims_pro_client_message->phone_number))
                                                                                                             @if($ims_pro_client_message->notify_name)
-                                                                                                                <div class="name">{{ get_contact_info($ims_pro_client_message->notify_name) }}</div>
+                                                                                                                <div class="name">{{ $ims_pro_client_message->notify_name }}</div>
                                                                                                             @else
                                                                                                                 <div class="name">{{ get_contact_info($ims_pro_client_message->phone_number) }}</div>
                                                                                                             @endif
