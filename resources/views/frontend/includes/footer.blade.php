4d2fb2b4f4053bbc567c9029c8e655bf98d472e8
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-05-14 21:16:56 +0530

    Footer Blade PHP Functions

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <section class="footer-section">
         <div class="container">
             <div class="row inner-wrapper g-0">
-                <div class="col-8">
+                <div class="col-9">
                     <div class="links">
                         <div class="header">
                             <a href="{{url('/')}}" rel="noopener noreferrer">
@@ -12,37 +12,51 @@
                             <div class="company">
                                 <ul class="navbar-nav">
                                     <li class="nav-item">Products</li>
-                                    <li class="nav-item"><a href="#" class="nav-link">All-in-One chat</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">WhatsApp Widget</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">SEO Tool</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Analytics Tool</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Security Tool</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">eCommerce Solutions</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">ERP Solutions</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">CRM Solutions</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/products/widget-lite" class="nav-link">WhatsApp widget</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/products/widget-pro" class="nav-link">All-in-One chat</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/products/optimizer" class="nav-link">Optimizer</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/products/optimizer" class="nav-link">Analytics Tool</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/products/optimizer" class="nav-link">SEO Tool</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/eshop" class="nav-link">eCommerce Solution</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/eshop" class="nav-link">LMS Solution</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/eshop" class="nav-link">CRM Solutions</a></li>
+                                </ul>
+                            </div>
+                            <div class="company">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">Services</li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">Digital Marketing</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">Mobile App Dev</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">Software Development</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">SEO</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">Web Designing</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">Graphic Designing</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">SEM</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">Social Media Marketing</a></li>
                                 </ul>
                             </div>
                             <div class="resources">
                                 <ul class="navbar-nav">
                                     <li class="nav-item">Services</li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Mobile App Development</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Software Development</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Digital Marketing</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">SEO</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">SEM</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/sitemap" class="nav-link">Mobile App Development</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/sitemap" class="nav-link">Navigation Map</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/expert-center" class="nav-link">Expert Centre</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/pricing" class="nav-link">Pricing</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/contact" class="nav-link">Help Centre</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/media/home-blog" class="nav-link">Blog</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/eshop" class="nav-link">Eshop</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/media/latest-news" class="nav-link">Latest news</a></li>
                                 </ul>
                             </div>
                             <div class="quick-links">
                                 <ul class="navbar-nav">
                                     <li class="nav-item">Company</li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Expert Center</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
                                     <li class="nav-item"><a href="#" class="nav-link">Careers</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Partners</a></li>
-                                    <li class="nav-item"><a href="{{url('media/home-blog')}}" class="nav-link">Blog</a></li>
-                                    <li class="nav-item"><a href="#" class="nav-link">Help center</a></li>
-                                    <li class="nav-item"><a href="{{url('terms-condition')}}" class="nav-link">Terms of Service</a></li>
-                                    <li class="nav-item"><a href="{{url('privacy-policy')}}" class="nav-link">Privacy Policy</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/contact" class="nav-link">Partners</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/portfolio" class="nav-link">Portfolio</a></li>
+                                    <li class="nav-item"><a href="https://tallentor.com/services/marketplace" class="nav-link">Marketplace</a></li>
+                                    <li class="nav-item"><a href="#" class="nav-link">Terms of use</a></li>
+                                    <li class="nav-item"><a href="#" class="nav-link">Privacy Policy</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -64,7 +78,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-3">
                     <div class="contact-info">
                         <div class="header">
                             <a href="https://web.whatsapp.com/send?phone=94760939990" target="_blank">
@@ -90,11 +104,11 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="footer">
+                        <div class="footer" style="padding: 20px;">
                             <div class="newsletter">
                                 <label for="newsletter"><button type="submit"
-                                        class="submit-btn">SUBSCRIBE</button></label>
-                                <input type="email" name="newsletter" id="newsletter" placeholder="Enter Your Email">
+                                        class="submit-btn" style="height: 32px;">SUBSCRIBE</button></label>
+                                <input style="height: 40px;" type="email" name="newsletter" id="newsletter" placeholder="Enter Your Email">
                             </div>
                         </div>
                     </div>
