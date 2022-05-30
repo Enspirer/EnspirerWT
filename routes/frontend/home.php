14d9e5534dc9f54671d68ae039810aad37929e8f
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-05-30 15:56:31 +0530

    Home PHP Payment Indicationror

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -56,7 +56,6 @@ Route::get('mobile_view/mobile-settings',[MobileViewController::class, 'mobile_s
 Route::get('mobile_view/mobile-register',[MobileViewController::class, 'mobile_register'])->name('mobile_register');
 Route::get('mobile_view/mobile-login',[MobileViewController::class, 'mobile_login'])->name('mobile_login');
 Route::get('mobile_view/mobile-analytics',[MobileViewController::class, 'mobile_analytics'])->name('mobile_analytics');
-Route::get('p/satus/{package_id}/{project_id}/{amount}', [PaymentController::class, 'getPaymentStatus'])->name('paypal.status');
 
 // Analytics Inner Pages
 Route::get('mobile_view/mobile-analytics/analytics-browsers',[MobileViewController::class, 'mobile_analytics_browsers'])->name('analytics_browsers');
@@ -154,6 +153,7 @@ Route::group(['middleware' => ['auth', 'password_expires']], function () {
         // User Dashboard Specific
         Route::get('p/checkout/{package_id}/{project_id}', [PaymentController::class, 'index'])->name('paypal.index');
         Route::post('p/checkout/pash', [PaymentController::class, 'postPaymentWithpaypal'])->name('paypal.show');
+        Route::get('p/satus/{package_id}/{project_id}/{amount}', [PaymentController::class, 'getPaymentStatus'])->name('paypal.status');
 
         Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
         Route::get('project_dash/delete/{id}', [DashboardController::class, 'project_dash_destroy'])->name('project_dash.destroy');
