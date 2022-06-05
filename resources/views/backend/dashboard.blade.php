c817b6d2da542ad8443cac5b9bd6d498c84147ff
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-06-05 23:58:52 +0530

    Dashboard Fucntion Blade Fucntion

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -6,13 +6,13 @@
 <style>
 .card-box {
     position: relative;
-    color: #fff;
+    color: #1b1b1b;
     padding: 20px 10px 40px;
     margin: 20px 0px;
 }
 .card-box:hover {
     text-decoration: none;
-    color: #f1f1f1;
+    color: #858585;
 }
 .card-box:hover .icon i {
     font-size: 100px;
@@ -69,22 +69,30 @@
     background-color: #d9534f !important;
 }
 </style>
-    <div class="container">
+
+<script type="text/javascript" src="https://fastly.jsdelivr.net/npm/echarts@5.3.2/dist/echarts.min.js"></script>
+
+
+
+
+
+
+    <div class="">
         <div class="row">
 
         @if(Module::has('Blog'))
             @if(Module::find('Blog')->isStatus(1))
 
                 <div class="col-lg-3 col-sm-6">
-                    <div class="card-box" style="background-color: #055147; border-radius: 15px 15px 15px 15px;">
+                    <div class="card-box" style="background-color: #ffffff;">
                         <div class="inner">
                             <h3>{{$category}}</h3>
-                            <p>Category</p>
+                            <p>Total Revenue</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-list mb-4 mr-2" aria-hidden="true"></i>
                         </div>
-                        <a href="{{url('admin/category')}}" class="card-box-footer" style="border-radius: 0px 0px 15px 15px;">View More <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="{{url('admin/category')}}" class="card-box-footer" style="background: #2275d7;">View More <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
             @else
@@ -108,7 +116,7 @@
             @if(Module::find('Blog')->isStatus(1))
 
                 <div class="col-lg-3 col-sm-6">
-                    <div class="card-box" style="background-color: #487662; border-radius: 15px 15px 15px 15px;">
+                    <div class="card-box" style="background-color: #ffffff;">
                         <div class="inner">
                             <h3>{{$post}}</h3>
                             <p>Post</p>
@@ -116,22 +124,22 @@
                         <div class="icon">
                             <i class="fas fa-newspaper mb-4 mr-2" aria-hidden="true"></i>
                         </div>
-                        <a href="{{url('admin/post')}}" class="card-box-footer" style="border-radius: 0px 0px 15px 15px;">View More <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="{{url('admin/post')}}" class="card-box-footer" style="background: #2275d7;">View More <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
 
             @else
 
                 <div class="col-lg-3 col-sm-6">
-                    <div class="card-box" style="background-color: #487662; border-radius: 15px 15px 15px 15px;">
+                    <div class="card-box" style="background-color: #ffffff;">
                         <div class="inner">
-                            <h3>Lorem</h3>
+                            <h3>Total Projects</h3>
                             <p>Lorem Ipsum</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-users mb-4 mr-2" aria-hidden="true"></i>
                         </div>
-                        <a href="" class="card-box-footer" style="border-radius: 0px 0px 15px 15px;">View More <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="" class="card-box-footer" style="background: #2275d7;">View More <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
 
@@ -140,7 +148,7 @@
 
           
             <div class="col-lg-3 col-sm-6">
-                <div class="card-box" style="background-color: #79a37d; border-radius: 15px 15px 15px 15px;">
+                <div class="card-box" style="background-color: #ffffff;">
                     <div class="inner">
                         <h3>Lorem</h3>
                         <p>Lorem Ipsum</p>
@@ -148,11 +156,11 @@
                     <div class="icon">
                         <i class="fas fa-city mb-4 mr-2" aria-hidden="true"></i>
                     </div>
-                    <a href="" class="card-box-footer" style="border-radius: 0px 0px 15px 15px;">View More <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="" class="card-box-footer" style="background: #2275d7;">View More <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <div class="col-lg-3 col-sm-6">
-                <div class="card-box" style="background-color: #92967d; border-radius: 15px 15px 15px 15px;">
+                <div class="card-box" style="background-color: #ffffff;">
                     <div class="inner">
                         <h3>Lorem</h3>
                         <p>Lorem Ipsum</p>
@@ -160,9 +168,108 @@
                     <div class="icon">
                         <i class="fas fa-building mb-4 mr-2"></i>
                     </div>
-                    <a href="" class="card-box-footer" style="border-radius: 0px 0px 15px 15px;">View More <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="" class="card-box-footer" style="background: #2275d7;">View More <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
         </div>
     </div>
+
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h4>Blog Details</h4><br>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h5>Blog Category</h5>
+                                            <h2>00</h2>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h5>Blog Posts</h5>
+                                            <h2>00</h2>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h5>Visit Count</h5>
+                                            <h2>00</h2>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                        </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="">
+                                        <div class="card-header">
+                                            Latest News
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="card">
+                                                <div class="card-body">
+                                                    <h4>Hello World Function</h4>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor sollicitudin lectus, ac viverra justo euismod at. Cras id dapibus diam, eu scelerisque tellus. </p>
+                                                <div class="row">
+                                                    <div class="col-md-6">
+
+                                                    </div>
+                                                    <div class="col-md-6" style="text-align: right">
+                                                        Created At : 2022/04/01
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <div class="col-md-6">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="">
+                        <h4>Billing Details</h4><br>
+
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor sollicitudin lectus, ac viverra justo euismod at. Cras id dapibus diam, eu scelerisque tellus.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="">
+                        <h4>Billing Details</h4><br>
+
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor sollicitudin lectus, ac viverra justo euismod at. Cras id dapibus diam, eu scelerisque tellus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
