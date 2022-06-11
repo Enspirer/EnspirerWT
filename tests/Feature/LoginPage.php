7600b15718b26d7086a49880de3b2bbb1ce056d1
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-06-12 01:42:27 +0530

    Login Page PHP Function

--- tests/Feature/LoginPage.php
+++ tests/Feature/LoginPage.php
@@ -6,18 +6,16 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class BasicTest extends TestCase
+class LoginPage extends TestCase
 {
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function home_page()
+    public function testExample()
     {
-        $response = $this->get('/');
+        $response = $this->get('/login');
         $this->assertTrue(true);
     }
-
-
 }
