1ea117bb97ba43a2daf71364f8fdfa8c7bcedda3
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-07-06 23:12:04 +0530

    mail

--- resources/views/frontend/mail/contactus_mail.blade.php
+++ resources/views/frontend/mail/contactus_mail.blade.php
@@ -48,7 +48,7 @@
                     <p style="color: black; margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Team Tallentor</p>
                   </td>
                 </tr>
-                <td style="padding:0 0 10px 0;color:#153643;" align="left">
+                <td style="padding:0 0 10px 0;color:#153643;" align="center">
                   <hr>
                   <p style="color: black; margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><b>Your Message</b></p>
                 </td> 
