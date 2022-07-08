77c9d3077f0997c44485db85f946d17520d13022
ACHii <achintha.tallentor@gmail.com>
2022-07-08 14:22:47 +0530

    footer

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <section class="footer-section">
         <div class="container">
             <div class="row inner-wrapper g-0">
-                <div class="col-9">
+                <div class="col-8">
                     <div class="links">
                         <div class="header">
                             <a href="{{url('/')}}" rel="noopener noreferrer">
@@ -60,7 +60,54 @@
                                 </ul>
                             </div>
                         </div>
-                        <div class="footer">
+                        <!-- <div class="footer">
+                            <div class="social-media">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item"><a href="https://www.facebook.com/tallentordigital/" class="nav-link" rel="noopener noreferrer" target="_blank"><i class="bi bi-facebook"></i></a>
+                                    </li>
+                                    <li class="nav-item"><a href="https://www.linkedin.com/company/tallentor/ " class="nav-link" rel="noopener noreferrer" target="_blank"><i class="bi bi-linkedin"></i></a>
+                                    </li>
+                                    <li class="nav-item"><a href="https://www.youtube.com/channel/UCwJgWOtpffBLBbYCgGtieVw" class="nav-link" rel="noopener noreferrer" target="_blank"><i class="bi bi-youtube"></i></a>
+                                    </li>
+                                    <li class="nav-item"><a href="https://www.instagram.com/tallentor/" class="nav-link" rel="noopener noreferrer" target="_blank"><i class="bi bi-instagram"></i></a>
+                                    </li>
+                                    <li class="nav-item"><a href="https://twitter.com/tallentor" class="nav-link" rel="noopener noreferrer" target="_blank"><i class="bi bi-twitter"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="contact-info">
+                        <div class="header">
+                            <a href="https://web.whatsapp.com/send?phone=94760939990" rel="noopener noreferrer" target="_blank">
+                                <img src="{{url('images/landing_page/home/phone-icon.png')}}" >
+                                <div class="title">Let's Talk</div>
+                            </a>
+                        </div>
+                        <div class="body">
+                            <div class="contact">
+                                <i class="bi bi-whatsapp"></i>
+                                <div class="block">
+                                    <div class="title">Hotline</div>
+                                    <a href="tel:+971503743699">+971 50 374 3699</a>
+                                </div>                               
+                            </div>
+                            <div class="contact">
+                                <i class="bi bi-envelope-check"></i>
+                                <div class="block">
+                                    <div class="title">Sales Inquiries</div>
+                                    <a href="mailto:hello@tallentordigital.com">sales@tallentordigital.com</a>
+                                </div>
+                            </div>
+                            <div class="contact">
+                                <i class="bi bi-send"></i>
+                                <div class="block">
+                                    <div class="title">General Inquiries</div>
+                                    <a href="mailto:hello@tallentordigital.com">hello@tallentordigital.com</a>
+                                </div>
+                            </div>
                             <div class="social-media">
                                 <ul class="navbar-nav">
                                     <li class="nav-item"><a href="https://www.facebook.com/tallentordigital/" class="nav-link" rel="noopener noreferrer" target="_blank"><i class="bi bi-facebook"></i></a>
@@ -76,48 +123,13 @@
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="contact-info">
-                        <div class="header">
-                            <a href="https://web.whatsapp.com/send?phone=94760939990" rel="noopener noreferrer" target="_blank">
-                                <img src="{{url('images/landing_page/home/phone-icon.png')}}" >
-                                <div class="title">Let's Talk</div>
-                            </a>
-                        </div>
-                        <div class="body">
-                            <div class="contact">
-                                <div class="header-block tel">
-                                    <i class="bi bi-whatsapp"></i>
-                                    <a href="tel:+971503743699">+971 50 374 3699</a>
-                                </div>
-                               
-                            </div>
-                            <div class="contact">
-                                <div class="header-block">
-                                    <i class="bi bi-envelope-check"></i>
-                                    <div class="title">Sales Inquiries</div>
-                                </div>
-                               
-                                <a href="mailto:hello@tallentordigital.com">sales@tallentordigital.com</a>
-                            </div>
-                            <div class="contact">
-                                <div class="header-block">
-                                    <i class="bi bi-send"></i>
-                                    <div class="title">General Inquiries</div>
-                                </div>
-                                
-                                <a href="mailto:hello@tallentordigital.com">hello@tallentordigital.com</a>
-                            </div>
-                        </div>
-                        <div class="footer" style="padding: 20px;">
+                        <!-- <div class="footer" style="padding: 20px;">
                             <div class="newsletter">
                                 <label for="newsletter"><button type="submit"
                                         class="submit-btn" style="height: 32px;">SUBSCRIBE</button></label>
                                 <input style="height: 40px;" type="email" name="newsletter" id="newsletter" placeholder="Enter Your Email">
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
